27223e616928e85e3fe520efcc0558c1da9cb304
Lucas Oliveira <lucas.g.oliveira1998@outlook.com>
2022-08-09 23:19:52 -0300

    Adicionando arquivo para construir tabelas sql

---
+++ whatchat.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE IF NOT EXISTS Whatchat;
+
+USE Whatchat;
+
+CREATE TABLE IF NOT EXISTS Usuarios
+(
+  Identificador int,
+  Nome varchar(50),
+  Email varchar(50),
+  Usertag varchar(25),
+  Senha varchar(25),
+  PRIMARY KEY(Identificador)
+);
+
+CREATE TABLE IF NOT EXISTS UsuarioContato
+(
+  IdentificadorUsuario1 int,
+  IdentificadorUsuario2 int,
+  PRIMARY KEY(IdentificadorUsuario1, IdentificadorUsuario2),
+  FOREIGN KEY(IdentificadorUsuario1) REFERENCES Usuarios(Identificador),
+  FOREIGN KEY(IdentificadorUsuario2) REFERENCES Usuarios(Identificador)
+);
+
+CREATE TABLE IF NOT EXISTS Mensagens
+(
+  Identificador int,
+  UsuarioAutorId int,
+  UsuarioReceptorId int,
+  MensagemHorario datetime,
+  MensagemConteudo varchar(250),
+  PRIMARY KEY (Identificador),
+  FOREIGN KEY (UsuarioAutorId) REFERENCES Usuarios(Identificador),
+  FOREIGN KEY (UsuarioReceptorId) REFERENCES Usuarios(Identificador)
+);
